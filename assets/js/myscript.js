e36fb5fb38c81b66f6d56d4da0bb963e324c2688
netcoredor <number6@ortizonline.com>
2018-04-20 16:40:07 -0400

    minor changes

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -23,59 +23,57 @@ var nodeJsCode = '';
 var pythonCode = '';
 var codes = [];
 var valueTypes = {};
-var jsonjadn = $.getJSON("assets/openc2.json", function (data) {
 
-	$.each(jsonjadn.responseJSON.types, function(i, item) {
-		valueTypes[item[0]] = item[1];
-	});
-	var actuator_specifiers = jsonjadn.responseJSON.types[7][4]; //retrieves actuator_specifiers
-	var unsortedActions = jsonjadn.responseJSON.types[4][4]; //retrieves actions
-	var unsortedTargets = jsonjadn.responseJSON.types[5][4]; //retrieves targets
-	var allTargetSpecifiers = jsonjadn.responseJSON.types[5][4];
-	codes['encryption_algorithm'] = jsonjadn.responseJSON.types[28][4];
-	codes['hashes'] = jsonjadn.responseJSON.types[27][4];
-	var allUnsortedActions = [];
-	i = 0;
-	while (i < unsortedActions.length) {
-		allUnsortedActions.push(unsortedActions[i][1]);
-		i++;
-	}
-	var allActions = allUnsortedActions.sort();
-	var allUnsortedTargets = [];
-	i = 0;
-	while (i < unsortedTargets.length) {
-		allUnsortedTargets.push(unsortedTargets[i][2]);
-		i++;
-	}
-	var allTargets = allUnsortedTargets.sort();
 
-	var allActuators = jsonjadn.responseJSON.types[6][4];
+	var jsonjadn = $.getJSON("assets/openc2.json", function (data) {
 
-	var allActuatorSpecifiers = jsonjadn.responseJSON.types[7][4];
-
-	$.each(allActions, function (i, item) {
-		$('#actionId').append('<a class="dropdown-item actionDropDownMenu" role="presentation" href="#" id=' + item + "SelectionId" + '>' + item + '</a>');
+		$.each(jsonjadn.responseJSON.types, function(i, item) {
+			valueTypes[item[0]] = item[1];
+		});
+		var actuator_specifiers = jsonjadn.responseJSON.types[7][4]; //retrieves actuator_specifiers
+		var unsortedActions = jsonjadn.responseJSON.types[4][4]; //retrieves actions
+		var unsortedTargets = jsonjadn.responseJSON.types[5][4]; //retrieves targets
+		var allTargetSpecifiers = jsonjadn.responseJSON.types[5][4];
+		codes['encryption_algorithm'] = jsonjadn.responseJSON.types[28][4];
+		codes['hashes'] = jsonjadn.responseJSON.types[27][4];
+		var allUnsortedActions = [];
+		i = 0;
+		while (i < unsortedActions.length) {
+			allUnsortedActions.push(unsortedActions[i][1]);
+			i++;
+		}
+		var allActions = allUnsortedActions.sort();
+		var allUnsortedTargets = [];
+		i = 0;
+		while (i < unsortedTargets.length) {
+			allUnsortedTargets.push(unsortedTargets[i][2]);
+			i++;
+		}
+		var allTargets = allUnsortedTargets.sort();
+	
+		var allActuators = jsonjadn.responseJSON.types[6][4];
+	
+		var allActuatorSpecifiers = jsonjadn.responseJSON.types[7][4];
+	
+		$.each(allActions, function (i, item) {
+			$('#actionId').append('<a class="dropdown-item actionDropDownMenu" role="presentation" href="#" id=' + item + "SelectionId" + '>' + item + '</a>');
+		});
+		$.each(allTargets, function (i, item) {
+			$('#targetId').append('<a class="dropdown-item targetDropDownMenu" role="presentation" href="#" id=' + item + "SelectionId" + '>' + item + '</a>');
+		});
+		$.each(allTargetSpecifiers, function (i, item) {
+			$('#targetSpecifiersId').append('<a class="dropdown-item targetSpecifierDropDownMenu" role="presentation" href="#" id=' + item[1] + "SelectionId" + '>' + item[1] + '</a>');
+		});
+		$.each(allActuators, function (i, item) {
+			$('#actuatorId').append('<a class="dropdown-item actuatorDropDownMenu" role="presentation" href="#" id=' + item[1] + "SelectionId" + '>' + item[1] + '</a>');
+		});
+		$.each(allActuatorSpecifiers, function (i, item) {
+			$('#actuatorSpecifierId').append('<a class="dropdown-item actuatorSpecifierDropDownMenu" role="presentation" href="#" id=' + item[1] + "SelectionId" + '>' + item[1] + '</a>');
+		});
 	});
-	$.each(allTargets, function (i, item) {
-		$('#targetId').append('<a class="dropdown-item targetDropDownMenu" role="presentation" href="#" id=' + item + "SelectionId" + '>' + item + '</a>');
-	});
-	$.each(allTargetSpecifiers, function (i, item) {
-		$('#targetSpecifiersId').append('<a class="dropdown-item targetSpecifierDropDownMenu" role="presentation" href="#" id=' + item[1] + "SelectionId" + '>' + item[1] + '</a>');
-	});
-	$.each(allActuators, function (i, item) {
-		$('#actuatorId').append('<a class="dropdown-item actuatorDropDownMenu" role="presentation" href="#" id=' + item[1] + "SelectionId" + '>' + item[1] + '</a>');
-	});
-	$.each(allActuatorSpecifiers, function (i, item) {
-		$('#actuatorSpecifierId').append('<a class="dropdown-item actuatorSpecifierDropDownMenu" role="presentation" href="#" id=' + item[1] + "SelectionId" + '>' + item[1] + '</a>');
-	});
-});
-$(document).ready(function () {
+	$(document).ready(function () {
 	$('.dropdown-item').on('click', (function () {
 		event.preventDefault();
-		//console.log(($(this).attr('class')));
-		//selectionClass = ($(this).attr('class').split(' '));
-		//selectionId = ($(this).attr('id'));
-		//selectedValue = this.text;
 		switch (($(this).attr('class').split(' '))[1]) {
 			case 'actionDropDownMenu':
 				actionFunction(this.text);
@@ -433,7 +431,7 @@ function createNewHashRow(inObject) {
 	$.each(codes['hashes'], function (i, item) {
 		$('#' + asteriskChecker + '_Menu').append('<a class="dropdown-item encryptoDropDownMenu extraHashContent newHashTypes" oc2name="target" oc2checkbox="' + asteriskChecker + '" oc2cmdname="' + $('#targetButtonId')[0].innerText + '" role="presentation" href="#" onclick="updateValues(this)">' + item[1] + '</a>');
 	});
-	$('#' + asteriskChecker + '_hashTD').after('<td class="newHashTypes extraHashContent"><input class="hashTypes inputString' + '" oc2name="target" oc2cmdname=' + $('#targetButtonId')[0].innerText + ' oc2checkbox="' + asteriskChecker + '" onchange="updateInputValues(this)" type="text" minlength="1" tabindex="-1" value=""/><i class="hashTypes fa fa-plus-circle hashContent" onclick="createNewHashRow(this)" style="color:rgb(40,167,69);font-size:46;"></i><i class="hashTypes fa fa-minus-circle hashContent" onclick="removeInputHash(this)" style="color:rgb(255,0,0);font-size:46;"></i></td>');
+	$('#' + asteriskChecker + '_hashTD').after('<td class="newHashTypes extraHashContent"><input class="hashTypes inputString' + '" oc2name="target" oc2cmdname=' + $('#targetButtonId')[0].innerText + ' oc2checkbox="' + asteriskChecker + '" onchange="updateInputValues(this)" type="text" minlength="1" tabindex="-1" value=""/><i class="hashTypes fa fa-minus-circle hashContent" onclick="removeInputHash(this)" style="color:rgb(255,0,0);font-size:46;"></i></td>');
 }
 function updateInputValues(inObject) {
 	if ($(inObject)["0"].attributes['oc2checkbox'].value == "hashes" || $(inObject)["0"].attributes['oc2checkbox'].value == "hash" || $(inObject)["0"].attributes['oc2checkbox'].value == "newhashes") {
